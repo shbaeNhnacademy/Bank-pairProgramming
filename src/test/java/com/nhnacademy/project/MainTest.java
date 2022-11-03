377e9eb81923b15e8e2abff7ae6b5bae14aab232
배수한 <qotngks152@naver.com>
2022-11-03 12:49:30 +0900

    test 폴더 생성

---
+++ src/test/java/com/nhnacademy/project/MainTest.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.project;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
